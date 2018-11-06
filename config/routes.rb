d2c0fb57a7852da433e3c5387a8943143ab78eeb
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-06 20:35:37 +0100

    fix #1 new author page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/authors/new' => 'authors#new'
+
+  resources :authors
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
