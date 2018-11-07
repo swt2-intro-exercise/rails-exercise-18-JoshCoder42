98e1b8eddbd882535ab4b149f16ca72b3548ddff
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-07 16:46:09 +0100

    slight testchanges for #4 due to syntax error and fixed failing tests #4 Author Model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe "New author page", type: :model do
 
 	it "should create an instance of an author" do
-		author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
-		expect(author.first_name).to_eq('Alan')
-		expect(author.last_name).to_eq('Turing')
-		expect(author.homepage).to_eq('http://wikipedia.org/Alan_Turing')
+		author = Author.new(first_name:'Alan', last_name:'Turing', homepage:'http://wikipedia.org/Alan_Turing')
+		expect(author.first_name).to eq('Alan')
+		expect(author.last_name).to eq('Turing')
+		expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+		expect(author.name).to eq('Alan Turing')
 	end
 
 end
