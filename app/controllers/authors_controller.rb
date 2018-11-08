5d59ac595730e3a5e22e5f006ebc251f4ea1c871
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-08 17:25:21 +0100

    fix failing test for #11 store update into db

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,18 +7,6 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-
- def create
-    @author = Author.new(author_params)
-
-    if @author.save
-      redirect_to @author
-    else
-      render "new"
-    end
- end
-
-
   def show
     @author = Author.find(params[:id])
   end
@@ -27,6 +15,26 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to @author
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+   
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render "edit"
+    end
+  end
   
 
   private
--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -13,12 +13,13 @@ describe "Edit author page", type: :feature do
  	it "should write updates into the database" do
  		visit edit_author_path(@alan)
  		
+ 		expect(@alan.first_name).to eq('Alan')
+
  		page.fill_in 'author[first_name]', with: 'Alan Mathison'
 	 	find('input[type="submit"]').click
 
 	 	@alan.reload
-
-	 	expect(Author.exists?({first_name: 'Alan Mathison', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing'})).not_to be_falsey
+	 	expect(@alan.first_name).to eq('Alan Mathison')
  	end
 
 end
