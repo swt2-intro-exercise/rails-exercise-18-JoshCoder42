46ef0ad7664675e91729887d7dc462dcd2e93cee
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-08 18:32:37 +0100

    add failing test for #18 paper input validation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,3 +3,12 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe "New paper page", type: :model do
+
+	it "should validate the input" do
+		@paper = Paper.new(venue:'mind 49: 433-460', year:2018)
+		expect(@paper).to_not be_valid
+	end
+
+end
