13d465c693c3adca5c2535c141e37c0267ed9b41
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-08 10:46:17 +0100

    fix failing test for #6 show author

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 describe "Show author page", type: :feature do
-
+	
+	before :each do
+        @alan = FactoryBot.create :author
+	end
+	
 	it "should show the details about an author" do
-		FactoryBot.create :author
 		visit author_path(@alan)
-		expect(page).to have_text('Alan')
-		expect(page).to have_text('Turing')
-		expect(page).to have_text('http://wikipedia.org/Alan_Turing')
+		expect(page).to have_text(@alan.first_name) 
+		expect(page).to have_text(@alan.last_name)
+		expect(page).to have_link href:(@alan.homepage)
 	end
 
 end
