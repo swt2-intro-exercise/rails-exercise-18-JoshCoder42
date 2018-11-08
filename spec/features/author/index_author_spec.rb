35b5172c7975eb7990628fdbe7edaa766bbf6c7a
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-08 17:53:11 +0100

    add failing test for #15 delete link on index page

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -18,7 +18,17 @@ describe "Index authors page", type: :feature do
 		expect(page).to have_text(@alan.last_name)
 		expect(page).to have_text(@alan.homepage)
 		expect(page).to have_link href:(author_path(@alan))
-		expect(page).to have_link href:(edit_author_path(@alan))
 	end
 
+	it "should have a link to edit an author" do
+		@alan = FactoryBot.create :author
+		visit authors_path
+		expect(page).to have_link href:(edit_author_path(@alan))
+	end 
+
+	it "should have a link to delete an author" do
+		@alan = FactoryBot.create :author
+		visit authors_path
+		expect(page).to have_css("a[data-method='delete'][href='/authors/#{@alan.id}']")
+	end 
 end
