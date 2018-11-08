a46c11dcb24078cd0147c783c06b07f239c27569
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-08 17:15:53 +0100

    add failing test for #11 store update into db

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -10,4 +10,15 @@ describe "Edit author page", type: :feature do
    		visit edit_author_path(@alan)
  	end
 
+ 	it "should write updates into the database" do
+ 		visit edit_author_path(@alan)
+ 		
+ 		page.fill_in 'author[first_name]', with: 'Alan Mathison'
+	 	find('input[type="submit"]').click
+
+	 	@alan.reload
+
+	 	expect(Author.exists?({first_name: 'Alan Mathison', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing'})).not_to be_falsey
+ 	end
+
 end
